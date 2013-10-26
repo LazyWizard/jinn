3bc3ca0696c971b75521dadc4c2e22d8c72a7e5a
LazyWizard <iu65a@hotmail.com>
2013-10-26 02:00:32 -0500

    VERY early WIP of Jinn stats and AI

--- org/lazywizard/jinn/JinnModPlugin.java
+++ org/lazywizard/jinn/JinnModPlugin.java
@@ -1,7 +1,24 @@
 package org.lazywizard.jinn;
 
 import com.fs.starfarer.api.BaseModPlugin;
+import com.fs.starfarer.api.PluginPick;
+import com.fs.starfarer.api.campaign.CampaignPlugin.PickPriority;
+import com.fs.starfarer.api.combat.ShipAIPlugin;
+import com.fs.starfarer.api.combat.ShipAPI;
+import com.fs.starfarer.api.fleet.FleetMemberAPI;
+import org.lazywizard.jinn.ai.JinnAI;
 
 public class JinnModPlugin extends BaseModPlugin
 {
+    @Override
+    public PluginPick<ShipAIPlugin> pickShipAI(FleetMemberAPI member, ShipAPI ship)
+    {
+        if (ship.getHullSpec().getHullId().startsWith("jinn_"))
+        {
+            return new PluginPick<ShipAIPlugin>(new JinnAI(ship),
+                    PickPriority.MOD_SPECIFIC);
+        }
+
+        return null;
+    }
 }
